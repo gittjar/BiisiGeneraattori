fd506e0f021e245d9ad78245dc9cb36d28885479
gittjar <jarno.keranen@gmail.com>
2022-11-01 20:24:53 +0200

    juuri luotu ja csv reader asennettu

---
+++ BiisiGeneraattori/Kappale.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization;
+using System;
+using System.Xml.Linq;
+using System.IO;
+using System.Reflection.Emit;
+using System.Formats.Asn1;
+using System.Data;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+using System.Drawing;
+
+namespace BiisiGeneraattori
+{
+    public class Kappale
+    {
+        public void printAll()
+        {
+
+            var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+            {
+                HasHeaderRecord = false,
+                Comment = '#',
+                AllowComments = true,
+                Delimiter = ";",
+            };
+
+            using var streamReader = File.OpenText("kappalelista.csv");
+            using var csvReader = new CsvReader(streamReader, csvConfig);
+
+            while (csvReader.Read())
+            {
+                // var Pos = csvReader.GetField(0);
+                var Artist = csvReader.GetField(1);
+                var Biisi = csvReader.GetField(2);
+                var Youtube = csvReader.GetField(3);
+
+                // Console.WriteLine($"{Row}# Albumi: {Artist} : {Album} --- Julkaistu: {Publish} --- Record Label: {Label}");
+                Console.WriteLine($"Artisti: {Artist} -- Kappale: {Biisi} -- Link to Youtube: {Youtube}");
+            }
+        }
+    }
+}
--- BiisiGeneraattori/Program.cs
+++ BiisiGeneraattori/Program.cs
@@ -1,3 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+
+namespace BiisiGeneraattori
+{
+    public class Program
+    {
+       
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Biisigeneraattori v.1.0");
+
+
+            while (true)
+            {
+                Console.WriteLine(" [ 1 ] = Kappalelista.\n [ 2 ] = Valitse satunnainen albumi." +
+                    "\n [ 3 ] = Lisää albumi.\n [ 4 ] = Poista albumi.\n [ 0 ] = Lopeta.");
+                string question = Console.ReadLine();
+
+
+
+                if (question == "1")
+                {
+
+                    Kappale albumit = new Kappale();
+                    albumit.printAll();
+                    Console.WriteLine("Press any key to continue!");
+                    Console.ReadKey();
+                }
+                else if (question == "2")
+                {
+                    Program sekoitus = new Program();
+                  //  sekoitus.ShuffleAlbum();
+                    Console.WriteLine("");
+                    Console.WriteLine("Press any key to continue!");
+                    Console.ReadKey();
+                }
+                else if (question == "3")
+                {
+                    Program lisaa = new Program();
+                 //   lisaa.AddAlbum();
+                    Console.WriteLine("");
+                    Console.WriteLine("Press any key to continue!");
+                    Console.ReadKey();
+                }
+                else if (question == "4")
+                {
+                    Program poista = new Program();
+                   // poista.RemoveAlbum();
+                    Console.WriteLine("");
+                    Console.WriteLine("Press any key to continue!");
+                    Console.ReadKey();
+                }
+
+
+                else if (question == "0")
+                {
+                    Console.WriteLine("You go exit!");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("You go exit!");
+                    Console.WriteLine("Press enter to quit.");
+                    break;
+                }
+
+            }
+            Console.WriteLine("paina Enter lopetaaksesi ohjelma.");
+            Console.ReadLine();
+
+
+        }
+
+
+
+
+    }
+}
